a020a1601a69a80ec0d2b1430ef03aba29509e3e
kimm <yasuihoro@mail.ru>
2023-08-07 13:34:58 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://decdf986-c004-4edd-a5c2-94f7c77ef1c1.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/" 
